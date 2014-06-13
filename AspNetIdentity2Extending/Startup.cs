fbdc8b729cec004fe7f3bda82b1ead6e82a111bf
Angel <jordanovski.angel@gmail.com>
2014-06-13 13:00:46 +0200

    Initial commit

---
+++ AspNetIdentity2Extending/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AspNetIdentity2Extending.Startup))]
+namespace AspNetIdentity2Extending
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
